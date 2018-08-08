ceece22352fff096e9a6e47abfb42e408d1bff46
Philippe Canal <pcanal@fnal.gov>
2018-08-08 07:56:32 -0500

    Work around limitation of Collection Proxy generator

--- persistency/root/RootPersistencyProxy.cpp
+++ persistency/root/RootPersistencyProxy.cpp
@@ -1,8 +1,12 @@
 #include "persistency/root/include/RootPersistencyProxy.h"
 #include <base/Array.h>
 
+#include <TVirtualCollectionProxy.h>
+#include <TGenCollectionStreamer.h>
+#include <TCollectionProxyFactory.h>
+
 template <class T>
-void SetCollectionProxy()
+void SetCollectionProxy(const char *altname)
 {
 
   auto info = ROOT::Detail::TCollectionProxyInfo::Generate(ROOT::Detail::TCollectionProxyInfo::Pushback<T>());
@@ -12,11 +16,30 @@ void SetCollectionProxy()
   }
   auto cl = TClass::GetClass(typeid(T));
 
+  TString keep = cl->GetName();
+
+  R__WRITE_LOCKGUARD(ROOT::gCoreMutex);
+
+  // Trick the collection in getting the right type.
+  cl->SetName(altname);
   if (cl) {
     cl->SetCollectionProxy(*info);
+    auto proxy = cl->GetCollectionProxy();
+    // Force initialization
+    auto type = proxy->GetCollectionType();
+    // fprintf(stderr, "Collection type is %d\n", type);
+
+    auto streamer = dynamic_cast<TCollectionClassStreamer *>(cl->GetStreamer());
+    if (streamer) {
+      proxy = streamer->GetXYZ();
+      type  = proxy->GetCollectionType();
+      // fprintf(stderr, "Collection Streamer type is %d\n", type);
+    }
+
   } else {
     fprintf(stderr, "Could not find the TClass for %s\n", typeid(T).name());
   }
+  cl->SetName(keep);
 
   delete info;
 }
@@ -26,6 +49,12 @@ void RootPersistencyProxy()
   // SetCollectionProxy<std::vector<const vecgeom::VPlacedVolume *>>();
   // SetCollectionProxy<std::vector<double>>();
   // SetCollectionProxy<std::vector<double *>>();
-  SetCollectionProxy<std::vector<double *>>();
   // SetCollectionProxy<vecgeom::Array<double>>();
+
+  SetCollectionProxy<vecgeom::Vector<const vecgeom::VPlacedVolume *>>("vector<const vecgeom::VPlacedVolume *>");
+  SetCollectionProxy<vecgeom::Vector<double>>("vector<double>");
+  SetCollectionProxy<vecgeom::Vector<double>>("vector<float>");
+  // This is is unlikely to be stored correctly as ROOT can't know the number of double pointed to by the double*.
+  // (it is *unlikely* to be just 1 as it is assumed for objects)
+  // SetCollectionProxy<vecgeom::Vector<double *>>("vector<double *>");
 }
