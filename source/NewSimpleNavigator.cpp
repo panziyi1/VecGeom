2a8869fe523063d4186088e23c1cd703c2a3796a
Philippe Canal <pcanal@fnal.gov>
2019-08-15 19:39:52 -0500

    Re-add inadvertently removed part of e4cdb6940be3ed223e401e01d270fddc5c3fe03b

---
+++ source/NewSimpleNavigator.cpp
@@ -0,0 +1,28 @@
+#include "navigation/NewSimpleNavigator.h"
+
+namespace vecgeom {
+inline namespace VECGEOM_IMPL_NAMESPACE {
+
+#if 0
+template <>
+NewSimpleNavigator<true> *NewSimpleNavigator<true>::fgInstance = nullptr;
+
+template <>
+NewSimpleNavigator<false> *NewSimpleNavigator<false>::fgInstance = nullptr;
+#endif
+
+#ifdef VECCORE_CUDA
+VECCORE_ATT_DEVICE
+VNavigator *gSimpleNavigator = nullptr;
+
+template <>
+VECCORE_ATT_DEVICE
+VNavigator *NewSimpleNavigator<false>::Instance()
+{
+  if (gSimpleNavigator == nullptr) gSimpleNavigator = new NewSimpleNavigator();
+  return gSimpleNavigator;
+}
+#endif
+
+} // namespace VECGEOM_IMPL_NAMESPACE
+} // namespace vecgeom
