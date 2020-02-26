b24b50944702b2a9365c32deb259e432c1dfa8fd
Andrei Gheata <Andrei.Gheata@cern.ch>
2020-02-26 17:17:11 +0100

    Temporarily inlined navigation in the Raytracer class

--- source/benchmarking/Raytracer.cpp
+++ source/benchmarking/Raytracer.cpp
@@ -9,10 +9,13 @@
 #include <VecGeom/navigation/NavigationState.h>
 #include <VecGeom/volumes/PlacedVolume.h>
 #include <VecGeom/management/GeoManager.h>
+
+/*
 #include <VecGeom/navigation/NewSimpleNavigator.h>
 #include <VecGeom/navigation/SimpleABBoxNavigator.h>
 #include <VecGeom/navigation/SimpleABBoxLevelLocator.h>
 #include <VecGeom/navigation/HybridNavigator2.h>
+*/
 
 #ifdef VECGEOM_CUDA_INTERFACE
 #include "VecGeom/backend/cuda/Backend.h"
@@ -79,12 +82,13 @@ void Raytracer::SetWorld(VPlacedVolumePtr_t world)
   fSourceDir.Normalize();
 
   // Create navigators (only for CPU case)
-  CreateNavigators();
+  //CreateNavigators();
 
   // Setup viewpoint state
   int maxdepth = GeoManager::Instance().getMaxDepth();
   fVPstate     = NavigationState::MakeInstance(maxdepth);
-  GlobalLocator::LocateGlobalPoint(fWorld, fStart, *fVPstate, true);
+  //GlobalLocator::LocateGlobalPoint(fWorld, fStart, *fVPstate, true);
+  LocateGlobalPoint(fWorld, fStart, *fVPstate, true);
 
   // Allocate rays
   fNrays                = fSize_px * fSize_py;
@@ -98,6 +102,209 @@ void Raytracer::SetWorld(VPlacedVolumePtr_t world)
   }
 }
 
+VPlacedVolume const *Raytracer::LocateGlobalPoint(VPlacedVolume const *vol, Vector3D<Precision> const &point,
+                                                  NavigationState &path, bool top) const
+{
+  VPlacedVolume const *candvolume = vol;
+  Vector3D<Precision> currentpoint(point);
+  if (top) {
+    assert(vol != nullptr);
+    if (!vol->UnplacedContains(point)) return nullptr;
+  }
+  path.Push(candvolume);
+  LogicalVolume const *lvol         = candvolume->GetLogicalVolume();
+  Vector<Daughter> const *daughters = lvol->GetDaughtersp();
+
+  bool godeeper = true;
+  while (daughters->size() > 0 && godeeper) {
+    for (size_t i = 0; i < daughters->size() && godeeper; ++i) {
+      VPlacedVolume const *nextvolume = (*daughters)[i];
+      Vector3D<Precision> transformedpoint;
+      if (nextvolume->Contains(currentpoint, transformedpoint)) {
+        path.Push(nextvolume);
+        currentpoint = transformedpoint;
+        candvolume   = nextvolume;
+        daughters    = candvolume->GetLogicalVolume()->GetDaughtersp();
+        break;
+      }
+    }
+    godeeper = false;
+  }
+  return candvolume;
+}
+
+VPlacedVolume const *Raytracer::LocateGlobalPointExclVolume(VPlacedVolume const *vol, VPlacedVolume const *excludedvolume,
+                                                            Vector3D<Precision> const &point, NavigationState &path, bool top) const
+{
+  VPlacedVolume const *candvolume = vol;
+  Vector3D<Precision> currentpoint(point);
+  if (top) {
+    assert(vol != nullptr);
+    candvolume = (vol->UnplacedContains(point)) ? vol : nullptr;
+  }
+  if (candvolume) {
+    path.Push(candvolume);
+    LogicalVolume const *lvol         = candvolume->GetLogicalVolume();
+    Vector<Daughter> const *daughters = lvol->GetDaughtersp();
+
+    bool godeeper = true;
+    while (daughters->size() > 0 && godeeper) {
+      godeeper = false;
+      // returns nextvolume; and transformedpoint; modified path
+      for (size_t i = 0; i < daughters->size(); ++i) {
+        VPlacedVolume const *nextvolume = (*daughters)[i];
+        if (nextvolume != excludedvolume) {
+          Vector3D<Precision> transformedpoint;
+          if (nextvolume->Contains(currentpoint, transformedpoint)) {
+            path.Push(nextvolume);
+            currentpoint = transformedpoint;
+            candvolume   = nextvolume;
+            daughters    = candvolume->GetLogicalVolume()->GetDaughtersp();
+            godeeper     = true;
+            break;
+          }
+        } // end if excludedvolume
+      }
+    }
+  }
+  return candvolume;
+}
+
+VPlacedVolume const *Raytracer::RelocatePointFromPathForceDifferent(Vector3D<Precision> const &localpoint,
+                                                                    NavigationState &path) const
+{
+  VPlacedVolume const *currentmother = path.Top();
+  VPlacedVolume const *entryvol      = currentmother;
+  if (currentmother != nullptr) {
+    Vector3D<Precision> tmp = localpoint;
+    while (currentmother) {
+      if (currentmother == entryvol || currentmother->GetLogicalVolume()->GetUnplacedVolume()->IsAssembly() ||
+          !currentmother->UnplacedContains(tmp)) {
+        path.Pop();
+        Vector3D<Precision> pointhigherup = currentmother->GetTransformation()->InverseTransform(tmp);
+        tmp                               = pointhigherup;
+        currentmother                     = path.Top();
+      } else {
+        break;
+      }
+    }
+
+    if (currentmother) {
+      path.Pop();
+      return LocateGlobalPointExclVolume(currentmother, entryvol, tmp, path, false);
+    }
+  }
+  return currentmother;  
+}
+
+double Raytracer::ComputeStepAndPropagatedState(Vector3D<Precision> const &globalpoint,
+                                                Vector3D<Precision> const &globaldir, Precision step_limit,
+                                                NavigationState const &in_state,
+                                                NavigationState &out_state) const
+{
+  // calculate local point/dir from global point/dir
+  // call the static function for this provided/specialized by the Impl
+  Vector3D<Precision> localpoint;
+  Vector3D<Precision> localdir;
+  // Impl::DoGlobalToLocalTransformation(in_state, globalpoint, globaldir, localpoint, localdir);
+  Transformation3D m;
+  in_state.TopMatrix(m);
+  localpoint = m.Transform(globalpoint);
+  localdir   = m.TransformDirection(globaldir);
+
+  Precision step                    = step_limit;
+  VPlacedVolume const *hitcandidate = nullptr;
+  auto pvol                         = in_state.Top();
+  auto lvol                         = pvol->GetLogicalVolume();
+
+  // need to calc DistanceToOut first
+  // step = Impl::TreatDistanceToMother(pvol, localpoint, localdir, step_limit);
+  step = pvol->DistanceToOut(localpoint, localdir, step_limit);
+  if (step < 0) step = 0;
+  // "suck in" algorithm from Impl and treat hit detection in local coordinates for daughters
+  //((Impl *)this)
+  //    ->Impl::CheckDaughterIntersections(lvol, localpoint, localdir, &in_state, &out_state, step, hitcandidate);
+  auto *daughters = lvol->GetDaughtersp();
+  auto ndaughters = daughters->size();
+  for (decltype(ndaughters) d = 0; d < ndaughters; ++d) {
+    auto daughter = daughters->operator[](d);
+    double ddistance = daughter->DistanceToIn(localpoint, localdir, step);
+
+    // if distance is negative; we are inside that daughter and should relocate
+    // unless distance is minus infinity
+    const bool valid = (ddistance < step && !IsInf(ddistance)) &&
+                       !((ddistance <= 0.) && in_state.GetLastExited() == daughter);
+    hitcandidate = valid ? daughter : hitcandidate;
+    step         = valid ? ddistance : step;
+  }
+
+  // fix state
+  bool done;
+  //step = Impl::PrepareOutState(in_state, out_state, step, step_limit, hitcandidate, done);
+    // now we have the candidates and we prepare the out_state
+  in_state.CopyTo(&out_state);
+  done = false;
+  if (step == kInfLength && step_limit > 0.) {
+    step = vecgeom::kTolerance;
+    out_state.SetBoundaryState(true);
+    do {
+      out_state.Pop();
+    } while (out_state.Top()->GetLogicalVolume()->GetUnplacedVolume()->IsAssembly());
+    done = true;
+  } else {
+    // is geometry further away than physics step?
+    // this is a physics step
+    if (step > step_limit) {
+      // don't need to do anything
+      step = step_limit;
+      out_state.SetBoundaryState(false);
+    } else {
+      // otherwise it is a geometry step
+      out_state.SetBoundaryState(true);
+      if (hitcandidate) out_state.Push(hitcandidate);
+
+      if (step < 0.) {
+        // std::cerr << "WARNING: STEP NEGATIVE; NEXTVOLUME " << nexthitvolume << std::endl;
+        // InspectEnvironmentForPointAndDirection( globalpoint, globaldir, currentstate );
+        step = 0.;
+      }
+    }
+  }
+
+  if (done) {
+    if (out_state.Top() != nullptr) {
+      assert(!out_state.Top()->GetLogicalVolume()->GetUnplacedVolume()->IsAssembly());
+    }
+    return step;
+  }
+  // step was physics limited
+  if (!out_state.IsOnBoundary()) return step;
+
+  // otherwise if necessary do a relocation
+  // try relocation to refine out_state to correct location after the boundary
+
+  // ((Impl *)this)->Impl::Relocate(MovePointAfterBoundary(localpoint, localdir, step), in_state, out_state);
+  localpoint += (step + 1.E-6) * localdir;
+
+  if (out_state.Top() == in_state.Top()) {
+    RelocatePointFromPathForceDifferent(localpoint, out_state);
+  } else {
+    // continue directly further down ( next volume should have been stored in out_state already )
+    VPlacedVolume const *nextvol = out_state.Top();
+    out_state.Pop();
+    LocateGlobalPoint(nextvol, nextvol->GetTransformation()->Transform(localpoint), out_state, false);
+  }
+
+  if (out_state.Top() != nullptr) {
+    while (out_state.Top()->IsAssembly()) {
+      out_state.Pop();
+    }
+    assert(!out_state.Top()->GetLogicalVolume()->GetUnplacedVolume()->IsAssembly());
+  }
+  return step;
+}
+
+/*
 void Raytracer::CreateNavigators()
 {
   // Create all navigators.
@@ -115,6 +322,7 @@ void Raytracer::CreateNavigators()
     lvol.second->SetLevelLocator(vecgeom::SimpleABBoxLevelLocator::GetInstance());
   }
 }
+*/
 
 void Raytracer::GenerateVolumePointers(VPlacedVolumePtr_t vol)
 {
@@ -124,6 +332,24 @@ void Raytracer::GenerateVolumePointers(VPlacedVolumePtr_t vol)
     // can check now the property of the conversions of *i
     GenerateVolumePointers(vd);
   }
+}
+
+int Raytracer::RaytraceOne(int px, int py, VPlacedVolumePtr_t world, ERTmodel model, ERTView view,
+                  Vector3D<double> start, Vector3D<double> dir, Vector3D<double> leftc, Vector3D<double> up, Vector3D<double> right, double scale,
+                  Vector3D<double> source_dir, NavigationState *vpstate)
+{
+  constexpr size_t buffer_size = 2048; // should contain current/next states
+  int maxdepth = GeoManager::Instance().getMaxDepth();
+  size_t navstate_size = NavigationState::SizeOfInstance(maxdepth);
+
+  char navstates_buffer[buffer_size];
+
+  Ray_t ray;
+  ray.fCrtState = NavigationState::MakeInstanceAt(maxdepth, (void *)(navstates_buffer));
+  ray.fNextState = NavigationState::MakeInstanceAt(maxdepth, (void *)(navstates_buffer + navstate_size));
+
+
+  return 0;
 }
 
 void Raytracer::StartRay(int iray)
@@ -141,7 +367,7 @@ void Raytracer::StartRay(int iray)
   ray.fDir.Normalize();
   ray.fColor  = 0xFFFFFFFF;
   ray.fVolume = (fView == kRTVperspective) ? fVPstate->Top()
-                                           : GlobalLocator::LocateGlobalPoint(fWorld, ray.fPos, *ray.fCrtState, true);
+                                           : /*GlobalLocator::*/LocateGlobalPoint(fWorld, ray.fPos, *ray.fCrtState, true);
   // Special case when starting point is outside the setup
   int itry = 0;
   while (!ray.fVolume && itry < kMaxTries) {
@@ -150,7 +376,7 @@ void Raytracer::StartRay(int iray)
     if (ray.fDone) return;
     // Propagate to the world volume (but do not increment the boundary count)
     ray.fPos += (snext + kPush) * ray.fDir;
-    ray.fVolume = GlobalLocator::LocateGlobalPoint(fWorld, ray.fPos, *ray.fCrtState, true);
+    ray.fVolume = /*GlobalLocator::*/LocateGlobalPoint(fWorld, ray.fPos, *ray.fCrtState, true);
   }
   *ray.fNextState = *ray.fCrtState;
   ray.fDone       = ray.fVolume == nullptr;
@@ -248,7 +474,7 @@ int Raytracer::PropagateOneStep(int iray)
   // Vector3D<double> start  = fLeftC + fScale * (px * fRight + py * fUp);
   Ray_t &ray              = fRays[px * fSize_py + py];
 
-  auto nav = ray.fVolume->GetLogicalVolume()->GetNavigator();
+  // auto nav = ray.fVolume->GetLogicalVolume()->GetNavigator();
   // auto nav     = static_cast<NewSimpleNavigator<false> *>(NewSimpleNavigator<false>::Instance());
   auto nextvol = ray.fVolume;
   double snext = kInfLength;
@@ -258,7 +484,7 @@ int Raytracer::PropagateOneStep(int iray)
     // if (px > 500 && px < 502 && py > 500 && py < 502) {
     //  std::cout << "ray " << iray << "\n";
     //}
-    snext   = nav->ComputeStepAndPropagatedState(ray.fPos, ray.fDir, kInfLength, *ray.fCrtState, *ray.fNextState);
+    snext   = /*nav->*/ComputeStepAndPropagatedState(ray.fPos, ray.fDir, kInfLength, *ray.fCrtState, *ray.fNextState);
     nextvol = ray.fNextState->Top();
     ray.fPos += (snext + kPush) * ray.fDir;
     nsmall++;
