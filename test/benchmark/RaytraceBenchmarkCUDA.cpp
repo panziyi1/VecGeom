3827bf75cba17c328bceae3a690da91f88a1497f
Andrei Gheata <Andrei.Gheata@cern.ch>
2020-04-22 14:53:56 +0200

    Changes to reproduce raytracing error in RelWithDebInfo mode (Debug works)

--- test/benchmark/RaytraceBenchmarkCUDA.cpp
+++ test/benchmark/RaytraceBenchmarkCUDA.cpp
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
    fprintf(stderr, "VecGeom must have GDML and CUDA enabled to run this example.\n");
    return 1;
 #else
-   int nx = 2048;
-   int ny = 1024;
+   int nx = 1;//2048;
+   int ny = 1;//1024;
 
    if (!vgdml::Frontend::Load(argv[1])) {
       fprintf(stderr, "Failed to open %s\n", argv[1]);
@@ -37,16 +37,16 @@ int main(int argc, char *argv[])
       return 3;
    }
 
-   int maxdepth = GeoManager::Instance().getMaxDepth() - 1;
+   int maxdepth = GeoManager::Instance().getMaxDepth();
 
    if (maxdepth <= 0)
       fprintf(stderr, "Error, geometry depth is reported as %d\n", maxdepth);
    else
       fprintf(stdout, "geometry depth is %d\n", maxdepth);
 
-   if (argv[2] && strcmp(argv[2], "CPU") == 0)
+   //if (argv[2] && strcmp(argv[2], "CPU") == 0)
      //RenderCPU(world, nx, ny, maxdepth);
-   else
+   //else
      RenderGPU((vecgeom::cuda::VPlacedVolume*)world, nx, ny, maxdepth);
 
    return 0;
