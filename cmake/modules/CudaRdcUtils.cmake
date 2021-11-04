1ef4d87bb20c500af08b521a6aa6c76bd658a880
Ben Morgan <ben.morgan@warwick.ac.uk>
2021-11-04 12:21:15 +0000

    Remove cuda_rdc_find_package_config
    This function is a simple wrapper, and it should be left up to clients
    to call `find_package_handle_standard_args` in CONFIG_MODE after `find_package`.
    
    Use of this command within a -Config.cmake file can lead to unexpected
    behaviour in combination with check_required_components. Revisit at a
    later date.

--- cmake/modules/CudaRdcUtils.cmake
+++ cmake/modules/CudaRdcUtils.cmake
@@ -87,31 +87,7 @@ relocatable device code.
   
   See ``install`` for additional detail.
   
-
-
-.. command:: cuda_rdc_find_package_config
-
-  ::
-
-    cuda_rdc_find_package_config(<package> [...])
-
-  Find the given package specified by a config file, but print location and
-  version information while loading. A well-behaved package Config.cmake file
-  should already include this, but several of the HEP packages (ROOT, Geant4,
-  VecGeom do not, so this helps debug system configuration issues.
-
-  The "Found" message should only display the first time a package is found and
-  should be silent on subsequent CMake reconfigures.
-
-  Once upstream packages are updated, this can be replaced by ``find_package``.
-
 #]=======================================================================]
-include(FindPackageHandleStandardArgs)
-
-macro(cuda_rdc_find_package_config _package)
-  find_package(${_package} CONFIG ${ARGN})
-  find_package_handle_standard_args(${_package} CONFIG_MODE)
-endmacro()
 
 define_property(TARGET PROPERTY CUDA_RDC_LIBRARY_TYPE
   BRIEF_DOCS "Indicate the type of cuda library (STATIC and SHARED for nvlink usage, FINAL for linking into not cuda library/executable"
